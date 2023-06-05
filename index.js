72a5f471b0cbb25554b2c8039845257051cc2d4e
Isaac Wanger <teryima127@gmail.com>
2023-06-05 11:05:37 +0100

    interactivity

---
+++ index.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+    const slider = document.querySelector(".slider");
+    const nextBtn = document.querySelector(".next-btn");
+    const prevBtn = document.querySelector(".prev-btn");
+    const slides = document.querySelectorAll(".slide");
+    const slideIcons = document.querySelectorAll(".slide-icon");
+    const numberOfSlides = slides.length;
+    var slideNumber = 0;
