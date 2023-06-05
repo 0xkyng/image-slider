caaead5b579d3ffa47bbcdeeb8f12ab7ab64b6f3
Isaac Wanger <teryima127@gmail.com>
2023-06-05 11:08:57 +0100

    previous button

--- index.js
+++ index.js
@@ -31,3 +31,14 @@
       slides[slideNumber].classList.add("active");
       slideIcons[slideNumber].classList.add("active");
     });
+
+//image slider previous button
+    prevBtn.addEventListener("click", () => {
+      slides.forEach((slide) => {
+        slide.classList.remove("active");
+      });
+      slideIcons.forEach((slideIcon) => {
+        slideIcon.classList.remove("active");
+      });
+
+      slideNumber--;
